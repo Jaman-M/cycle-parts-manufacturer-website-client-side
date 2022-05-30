cceafddeab5f8f6d011987f68dff20d792c532c8
Jaman-M <jaman.manik1@gmail.comm>
2022-05-30 23:51:20 +0600

    added my orders and review

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,11 +1,91 @@
-import React from 'react';
+
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import OrderDetails from "./OrderDetails";
 
 const MyOrders = () => {
+    const [user] = useAuthState(auth);
+    const [products, setProducts] = useState([]);
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders/user/${user.email}`)
+          .then(res => res.json())
+            .then(data => setProducts(data));
+    }, [user.email])
+
+    const email = user.email;
+
+    const handleDelate = id => {
+        const rest = products.filter(product => product._id !== id)
+        const proceed = window.confirm('Are You Sure Cancel This ???')
+        if (proceed) {
+            const url = (`http://localhost:5000/orders/orders/${id}`)
+                fetch(url, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        setProducts(rest)
+
+                    })
+        }
+    }
+
     return (
         <div>
-            <h2>my orders</h2>
+    
+          <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Product name
+                                </th>
+                                
+                                
+                                <th scope="col" class="px-6 py-3">
+                                    email
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    price
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    quantity
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Pay Now
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Cancel Order
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                                        products.map(order =><OrderDetails
+                                          key={order._id}
+                                          order={order}
+                                          handleDelate={handleDelate}
+                                        ></OrderDetails>)
+                                    }
+    
+                            
+    
+    
+    
+                        </tbody>
+                    </table>
+                </div>
+         
         </div>
-    );
+      );
+    
+    
+    
+    
 };
 
 export default MyOrders;
--- src/Pages/Dashboard/Review.js
+++ src/Pages/Dashboard/Review.js
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
 
 const Review = () => {
+    const { register, handleSubmit } = useForm();
+    const params = useParams();
+    const [singleProduct, setSingleProduct] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/parts/${params.id}`)
+          .then((res) => res.json())
+          .then((data) => setSingleProduct(data));
+      }, []);
+    const onSubmit = data => {
+        const order = {
+            name: data.name,
+            description: data.description,
+            img: data.img,
+            ratting: data.ratting,
+        }
+        fetch('http://localhost:5000/review', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+        .then(res =>res.json())
+        .then(data =>{
+            console.log(data)
+        })
+    }
     return (
         <div>
-            <h2>Add a review</h2>
+            <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                <div className="card-body bg-base-100 shadow-xl border-blue-900 border-2">
+                <form onSubmit={handleSubmit( onSubmit)}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              className="input border-blue-900 border-2"
+              {...register("name", {
+              })}
+            />
+            <label className="label">
+              <span className="label-text">Image</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Url"
+              className="input border-blue-900 border-2"
+              {...register("img", {
+              })}
+            />
+            <label className="label">
+              <span className="label-text">Description</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Type Your Review"
+              className="input border-blue-900 border-2"
+              {...register("description", {
+              })}
+            />
+            <label className="label">
+              <span className="label-text">Ratting</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Select Your Ratting"
+              className="input border-blue-900 border-2"
+              {...register("ratting", {
+              })}
+            />
+            <div className="mt-3 text-center">
+            <input type="submit" className="btn btn-outline btn-secondary h-2 w-32" />
+            </div>
+          </div>
+             </form>
+                </div>
+            </div>
         </div>
     );
 };
